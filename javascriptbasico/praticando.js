80908b5ea915973fd1beb86bebbe91fd1f7b5c69
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-03-29 15:26:02 -0300

    29/03

--- javascriptbasico/praticando.js
+++ javascriptbasico/praticando.js
@@ -1,9 +1,16 @@
-var a = 3;
+var idade = 9
 
-var b = "4";
+var menor10 = idade <= 10;
+var maior65 = idade >=65;
 
+var gratuidade = menor10 || maior65
 
-console.log( a !== b);
+console.log("Idade", idade);
 
+console.log("Maior que 65 ", maior65);
+
+console.log("Menor que 10 ", menor10);
+
+console.log("Tem direto a gratuidade", gratuidade);
 
  
