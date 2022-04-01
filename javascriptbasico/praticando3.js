4e42a7a9aafd3019f727727067ba5378d7e128b6
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-04-01 11:55:43 -0300

    01/04/22

---
+++ javascriptbasico/praticando3.js
@@ -0,0 +1,13 @@
+var idade = 71;
+//----------------------------------
+
+if (idade >= 18 && idade <=70){
+    console.log("pode");
+    console.log("Qual seu pedido");
+}
+
+else {
+    console.log("não pode");
+    console.log("Volte futuramente");
+}
+//----------------------------------
