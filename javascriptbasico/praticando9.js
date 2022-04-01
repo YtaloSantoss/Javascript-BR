e9fdb80e599864c959a37f34d4a4181b061a8ad8
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-04-01 19:35:29 -0300

    01/04/22

---
+++ javascriptbasico/praticando9.js
@@ -0,0 +1,11 @@
+var alunos = ["Igor", "José", "Marcos", "Maria", "João"]
+
+//vai pega cada posição dentro da variavel a cada vez que entrar no laço com o "in"  indeces 
+// e com o of pega os elementos
+for (var entrou in alunos) {
+    console.log(alunos[entrou])
+}
+
+/*for (var entrou of alunos) {
+    console.log(entrou)
+}*/
