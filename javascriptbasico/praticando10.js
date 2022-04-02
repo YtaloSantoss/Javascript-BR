d631dcdee2dcc28ea5d3216c3efcbe353d7d28a3
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-04-02 13:14:13 -0300

    02/04/22

---
+++ javascriptbasico/praticando10.js
@@ -0,0 +1,10 @@
+function media(n1,n2) {
+    var nota1 = n1;
+    var nota2 = n2
+    var media = (nota1 + nota2) / 2
+//  console.log(media)
+    return media
+}
+
+var resultado = media(6, 7);
+console.log(resultado)
