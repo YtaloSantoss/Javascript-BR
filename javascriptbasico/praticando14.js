f046c7a5868af58a41bd84c0f010293226e4fa6c
Ytalo Mateus Silva Santos <99446046+YtaloSantoss@users.noreply.github.com>
2022-04-02 16:44:19 -0300

    02/04/22

---
+++ javascriptbasico/praticando14.js
@@ -0,0 +1,15 @@
+
+/* var aluno = {nome: "João", nota1: [7.5, 5.0]}
+
+aluno.matricula = 12345;
+
+aluno["sobrenome"] = "Oliveira";
+
+console.log(aluno) */
+
+var aluno = new Object();
+
+aluno.nome = "igor";
+aluno.notas = [8, 9];
+
+console.log(aluno)
